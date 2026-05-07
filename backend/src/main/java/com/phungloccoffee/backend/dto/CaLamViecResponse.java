8b2d217563f65792fe07212bd19a9426d604792b
Khoa Huynh <khoahuynh2106@gmail.com>
2026-05-07 18:27:38 +0700

    add: dto CaLamViecReponse.java

---
+++ backend/src/main/java/com/phungloccoffee/backend/dto/CaLamViecResponse.java
@@ -0,0 +1,25 @@
+package com.phungloccoffee.backend.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CaLamViecResponse {
+    
+    private String maCa;
+    private LocalDate ngayLamViec;
+    private LocalTime gioBatDau;
+    private LocalTime gioKetThuc;
+    private Integer trangThai;
+    
+    private String maNV;
+    private String tenNV;
+}
