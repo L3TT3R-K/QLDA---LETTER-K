212c326216256a1654c4596b1c6c4750a8ced292
khoitang-tech <24520868@gm.uit.edu.vn>
2026-05-09 20:59:27 +0700

    Them entity HoaDon

---
+++ backend/src/main/java/com/phungloccoffee/backend/entity/HoaDon.java
@@ -0,0 +1,52 @@
+package com.phungloccoffee.backend.entity;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "hoadon")
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class HoaDon {
+
+    @Id
+    @Column(name = "mahd", length = 50)
+    private String maHD;
+
+    @Column(name = "maca", length = 50)
+    private String maCa;
+
+    @ManyToOne
+    @JoinColumn(name = "macn")
+    private ChiNhanh chiNhanh;
+
+    @Column(name = "tongtien")
+    private BigDecimal tongTien;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "trangthai")
+    private Integer trangThai;
+
+    @Column(name = "issynced")
+    private Boolean isSynced;
+}
