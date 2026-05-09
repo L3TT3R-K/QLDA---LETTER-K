8f72b25efbc2a6da49990d6fb2fb617cd6059685
khoitang-tech <24520868@gm.uit.edu.vn>
2026-05-09 22:07:28 +0700

    Them Repository ThanhToanRepository

---
+++ backend/src/main/java/com/phungloccoffee/backend/repository/ThanhToanRepository.java
@@ -0,0 +1,9 @@
+package com.phungloccoffee.backend.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.phungloccoffee.backend.entity.ThanhToan;
+
+@Repository
+public interface ThanhToanRepository extends JpaRepository<ThanhToan, String> {
+}
