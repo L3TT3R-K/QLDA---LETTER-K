c0c7382cbc96d0a6b8fcb9eabf77a23e6ea7c047
khoitang-tech <24520868@gm.uit.edu.vn>
2026-05-10 22:34:44 +0700

    Update CTKKRepository

--- backend/src/main/java/com/phungloccoffee/backend/repository/CTKKRepository.java
+++ backend/src/main/java/com/phungloccoffee/backend/repository/CTKKRepository.java
@@ -1,8 +1,12 @@
 package com.phungloccoffee.backend.repository;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.phungloccoffee.backend.entity.CTKK;
 import com.phungloccoffee.backend.entity.CTKK_ID;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CTKKRepository extends JpaRepository<CTKK, CTKK_ID> {
+    //Lấy chi tiết đếm kho của 1 mã phiếu kiểm
+    List<CTKK> findByMaKK(String maKK);
 }
