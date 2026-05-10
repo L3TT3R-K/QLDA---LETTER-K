02abb131c34a4690cb481137be3fd146fefae417
khoitang-tech <24520868@gm.uit.edu.vn>
2026-05-10 14:52:54 +0700

    Them ham tim kiem sl ton kho hien tai

--- backend/src/main/java/com/phungloccoffee/backend/repository/TonKhoRepository.java
+++ backend/src/main/java/com/phungloccoffee/backend/repository/TonKhoRepository.java
@@ -1,8 +1,11 @@
 package com.phungloccoffee.backend.repository;
 
-import com.phungloccoffee.backend.entity.TonKho;
-import com.phungloccoffee.backend.entity.TonKho_ID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.phungloccoffee.backend.entity.TonKho;
+import com.phungloccoffee.backend.entity.TonKho_ID;
+
 public interface TonKhoRepository extends JpaRepository<TonKho, TonKho_ID> {
+    //Hàm tìm kiếm số lượng tồn kho hiện tại
+    TonKho findByMaCNAndMaNL(String maCN, String maNL);
 }
