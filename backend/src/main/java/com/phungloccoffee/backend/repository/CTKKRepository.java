d81733d8099808fb53bff5917e7849907b78c6ac
Khoa Huynh <khoahuynh2106@gmail.com>
2026-05-10 09:06:30 +0700

    add repo cho CTKK và KiemKho

---
+++ backend/src/main/java/com/phungloccoffee/backend/repository/CTKKRepository.java
@@ -0,0 +1,8 @@
+package com.phungloccoffee.backend.repository;
+
+import com.phungloccoffee.backend.entity.CTKK;
+import com.phungloccoffee.backend.entity.CTKK_ID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CTKKRepository extends JpaRepository<CTKK, CTKK_ID> {
+}
---
+++ backend/src/main/java/com/phungloccoffee/backend/repository/KiemKhoRepository.java
@@ -0,0 +1,7 @@
+package com.phungloccoffee.backend.repository;
+
+import com.phungloccoffee.backend.entity.KiemKho;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface KiemKhoRepository extends JpaRepository<KiemKho, String> {
+}
