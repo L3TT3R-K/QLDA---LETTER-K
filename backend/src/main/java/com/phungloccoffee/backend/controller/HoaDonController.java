c5f7f310e91880f6154dd67849308ef02b5ba924
khoitang-tech <24520868@gm.uit.edu.vn>
2026-05-10 09:45:40 +0700

    Update HoaDonController

--- backend/src/main/java/com/phungloccoffee/backend/controller/HoaDonController.java
+++ backend/src/main/java/com/phungloccoffee/backend/controller/HoaDonController.java
@@ -1,10 +1,13 @@
 package com.phungloccoffee.backend.controller;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.phungloccoffee.backend.dto.ChiTietBillResponse;
 import com.phungloccoffee.backend.dto.HoaDonRequest;
 import com.phungloccoffee.backend.dto.HoaDonResponse;
 import com.phungloccoffee.backend.service.HoaDonService;
@@ -18,8 +21,14 @@ public class HoaDonController {
     @PostMapping
     public HoaDonResponse taoHoaDon(
         @RequestBody HoaDonRequest request
-    ) 
-    {
+    ) {
         return service.taoHoaDon(request);
+    }
+
+    @GetMapping("/{maHD}")
+    public ChiTietBillResponse getChiTietHoaDon(
+        @PathVariable String maHD
+    ) {
+        return service.getChiTietHoaDon(maHD);
     }
 }
