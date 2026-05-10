88496ff973ec5b66902987f8650c290419c7a42e
khoitang-tech <24520868@gm.uit.edu.vn>
2026-05-10 09:19:01 +0700

    Update CTHDRepository

--- backend/src/main/java/com/phungloccoffee/backend/repository/CTHDRepository.java
+++ backend/src/main/java/com/phungloccoffee/backend/repository/CTHDRepository.java
@@ -1,10 +1,13 @@
 package com.phungloccoffee.backend.repository;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.phungloccoffee.backend.entity.CTHD;
+import com.phungloccoffee.backend.entity.HoaDon;
 
 @Repository
 public interface CTHDRepository extends JpaRepository<CTHD, String> {
+    List<CTHD> findByHoaDon(HoaDon hoaDon);
 }
