9409ca0b3358372db019e5401465bbc35bbfcad9
khoitang-tech <24520868@gm.uit.edu.vn>
2026-05-10 22:35:17 +0700

    Update KiemKhoRepository

--- backend/src/main/java/com/phungloccoffee/backend/repository/KiemKhoRepository.java
+++ backend/src/main/java/com/phungloccoffee/backend/repository/KiemKhoRepository.java
@@ -1,7 +1,12 @@
 package com.phungloccoffee.backend.repository;
+import java.time.LocalDateTime;
+import java.util.List;
 
-import com.phungloccoffee.backend.entity.KiemKho;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.phungloccoffee.backend.entity.KiemKho;
+
 public interface KiemKhoRepository extends JpaRepository<KiemKho, String> {
+    //Tìm các phiếu kiểm kho của 1 chi nhánh trong khoảng thời gian
+    List<KiemKho> findByChiNhanh_MaCNAndNgayKiemBetween(String maCN, LocalDateTime tuNgay, LocalDateTime denNgay);
 }
