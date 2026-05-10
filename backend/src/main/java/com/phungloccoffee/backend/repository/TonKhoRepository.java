8278eecb7d59f058c3d927d8d2cd71bb41b522f6
khoitang-tech <24520868@gm.uit.edu.vn>
2026-05-10 22:35:44 +0700

    Update TonKhoRepository

--- backend/src/main/java/com/phungloccoffee/backend/repository/TonKhoRepository.java
+++ backend/src/main/java/com/phungloccoffee/backend/repository/TonKhoRepository.java
@@ -1,4 +1,5 @@
 package com.phungloccoffee.backend.repository;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,4 +9,6 @@ import com.phungloccoffee.backend.entity.TonKho_ID;
 public interface TonKhoRepository extends JpaRepository<TonKho, TonKho_ID> {
     //Hàm tìm kiếm số lượng tồn kho hiện tại
     TonKho findByMaCNAndMaNL(String maCN, String maNL);
+    //Lấy toàn bộ nguyên liệu trong kho của 1 chi nhánh
+    List<TonKho> findByMaCN(String maCN);
 }
