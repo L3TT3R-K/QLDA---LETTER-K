caa625576d0c78a29d20fb9481f53bdd9b5c9dff
Khoa Huynh <khoahuynh2106@gmail.com>
2026-05-28 10:34:03 +0700

    sửa lại mockdata cho nhanvien

--- postgres/mockdata.sql
+++ postgres/mockdata.sql
@@ -22,13 +22,9 @@ INSERT INTO nhacungcap (mancc, tenncc, trangthai) VALUES
 -- Đã tích hợp Username và PasswordHash vào bảng NhanVien
 INSERT INTO nhanvien (manv, tennv, chucvu, macn, username, passwordhash, trangthai) VALUES
 ('NV001','Nguyễn Văn An','ADMIN',NULL,'admin','$2a$10$QG.9XTiVePNgbBGdan2/IeXFUnObAP24c7rbS5/pLa87umZ7vB3de', 1),
-('NV002','Trần Thị Bình','QUANLY_CHINHANH','CN01','manager_cn01','$2a$10$DowJonesDemoHashFor123456xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx', 1),
 ('NV003','Lê Văn Cường','NHANVIEN_BANHANG','CN01','cashier_cn01','$2a$10$DowJonesDemoHashFor123456xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx', 1),
-('NV004','Phạm Thị Dung','PHA_CHE','CN01',NULL,NULL, 1),
 ('NV005','Hoàng Văn Em','NHANVIEN_KHO','CN01','stock_cn01','$2a$10$DowJonesDemoHashFor123456xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx', 1),
-('NV006','Đỗ Thị Hoa','QUANLY_CHINHANH','CN02','manager_cn02','$2a$10$DowJonesDemoHashFor123456xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx', 1),
-('NV007','Võ Văn Khang','NHANVIEN_BANHANG','CN02','cashier_cn02','$2a$10$DowJonesDemoHashFor123456xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx', 1),
-('NV008','Ngô Thị Lan','KETOAN',NULL,'ketoan','$2a$10$DowJonesDemoHashFor123456xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx', 1);
+('NV007','Võ Văn Khang','NHANVIEN_BANHANG','CN02','cashier_cn02','$2a$10$DowJonesDemoHashFor123456xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx', 1);
 
 INSERT INTO sanpham (masp, tensp, giahientai, istopping, trangthai) VALUES
 ('SP001','Cà phê đen đá',25000,FALSE, 1),
